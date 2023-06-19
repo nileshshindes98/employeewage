c432aeef427179a247c4d1832ebddb9ef99c3d2f
Nilesh Shinde <nileshshindes98@gmail.com>
2023-06-19 06:29:11 +0530

    Added : Branch 5

--- Empwage.java
+++ Empwage.java
@@ -4,24 +4,31 @@ public class Empwage {
     public static final int IS_FULL_TIME = 2;
     public static final int IS_PART_TIME = 1;
     public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS =20;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 
 
         int empHrs = 0;
         int empWage = 0;
-        int empCheck = (int)Math.floor(Math.random()*10)%3;
-        switch ( empCheck) {
-            case IS_PART_TIME :
-                empHrs = 4;
-                break ;
-            case IS_FULL_TIME :
-                empHrs = 8;
-            default :
-                empHrs = 0;
-        }
+        int totalEmpWage = 0;
+        for (int day=0;day<NUM_OF_WORKING_DAYS;day++){
 
-        empWage = empHrs*EMP_RATE_PER_HOUR;
+            int empCheck = (int)Math.floor(Math.random()*10)%3;
+            switch ( empCheck) {
+                case IS_PART_TIME :
+                    empHrs = 4;
+                    break ;
+                case IS_FULL_TIME :
+                    empHrs = 8;
+                default :
+                    empHrs = 0;
+            }
+
+            empWage = empHrs*EMP_RATE_PER_HOUR;
+            totalEmpWage+=empWage;
             System.out.println("Employee wage : "+empWage);
+        }
+        System.out.println("Total Emp Wage : "+totalEmpWage);
     }
 }
